bb9161a69e6bdd7d9c982d8d236baa9a0c49a994
molingyu <z1522716486@hotmail.com>
2018-09-04 22:45:38 +0800

    feat: add database & dataSystem

---
+++ Assets/Data/DataBase.cs
@@ -0,0 +1,9 @@
+﻿using Sirenix.OdinInspector;
+
+namespace Assets.Data
+{
+    public class Database : SerializedScriptableObject
+    {
+        public DataSystem System;
+    }
+}
---
+++ Assets/Data/DataSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sirenix.OdinInspector;
+
+namespace Assets.Data
+{
+    [Serializable]
+    public enum Scenes
+    {
+        TitleScene = 1,
+        MapScene = 2,
+        StaffScene = 3,
+    }
+
+    public class DataSystem : SerializedScriptableObject
+    {
+        public Scenes FirstScene;
+    }
+}
